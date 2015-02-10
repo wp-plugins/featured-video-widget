51db1c0cf9338697b6e5fa97b6f570b0e9dd6ce9
Qassim.Dev <Qassim.Dev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-10 22:27:00 +0000

    featured-video-widget readme
    git-svn-id: https://plugins.svn.wordpress.org/featured-video-widget/trunk@1086730 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ featured-video-widget.php
@@ -0,0 +1,121 @@
+<?php
+/*
+Plugin Name: Featured Video Widget
+Plugin URI: http://qass.im/my-plugins/
+Description: Add featured Youtube or Vimeo video in your sidebar easily, responsive and customize height.
+Version: 1.0.0
+Author: Qassim Hassan
+Author URI: http://qass.im/
+License: GPLv2 or later
+*/
+
+/*  Copyright 2014  Qassim Hassan  (email : qassim.pay@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+// Featured Video Widget
+class QassimFeaturedVideoWidget extends WP_Widget {
+	function QassimFeaturedVideoWidget() {
+		parent::__construct( false, 'Featured Video', array('description' => 'Display featured video from youtube or vimeo.') );
+	}
+
+	function widget( $args, $instance ) {
+		extract( $args );
+		$title = $instance['title'];
+		$video = $instance['videolink'];
+		$height = $instance['height'];
+
+		if( empty($title) ){
+			$title = 'Featured Video';
+		}
+		if( empty($height) ){
+			$height = '220';
+		}
+
+		?>
+            <?php 
+			if( !empty($video) ){
+				if( preg_match("/(youtube)+/", $video) or preg_match("/(youtu.be)+/", $video) ){
+					$protocol 	= array('http://', 'https://', 'www.', 'youtube.com', 'youtu.be', 'embed', 'watch?v=', '/');
+					$video_link = str_replace($protocol, '', $video);
+					$the_result = '<iframe style="width:100%;max-width:100%;display:block;height:'.$height.'px;" src="http://youtube.com/embed/'.$video_link.'" allowfullscreen></iframe>';
+				}
+				elseif( preg_match("/(vimeo)+/", $video) ){
+					$protocol 	= array('http://', 'https://', 'www.', 'vimeo.com', '/');
+					$video_link = str_replace($protocol, '', $video);
+					$the_result = '<iframe style="width:100%;max-width:100%;display:block;height:'.$height.'px;" src="http://player.vimeo.com/video/'.$video_link.'" allowfullscreen></iframe>';
+				}else{
+					$the_result = '<ul><li>Error video link! Please add youtube or vimeo video link only.</li></ul>';
+				}
+			}else{
+				$the_result = '<ul><li>Please add youtube or vimeo video link.</li></ul>';
+			}
+			?>
+            
+			<?php echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title']; ?>
+            
+			<?php echo $the_result; ?>
+            
+            <?php echo  $args['after_widget']; ?>
+        <?php
+	}//widget
+	
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['videolink'] = strip_tags($new_instance['videolink']);
+		$instance['height'] = strip_tags($new_instance['height']);
+		return $instance;
+	}//update
+	
+	function form( $instance ) {
+		$instance = wp_parse_args(
+			(array) $instance
+		);
+		
+		$defaults = array(
+			'title' 	=> 'Featured Video',
+			'videolink' => '',
+			'height' 	=> '220'
+		);
+		
+		$instance = wp_parse_args( (array) $instance, $defaults );
+		$title = $instance['title'];
+		$videolink = $instance['videolink'];
+		$height = $instance['height'];
+		?>
+			<p>
+				<label for="<?php echo $this->get_field_id('title'); ?>">Title:</label> 
+				<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+			</p>
+            
+			<p>
+				<label for="<?php echo $this->get_field_id('videolink'); ?>">Video Link:</label> 
+				<input class="widefat" id="<?php echo $this->get_field_id('videolink'); ?>" name="<?php echo $this->get_field_name('videolink'); ?>" type="text" value="<?php echo $videolink; ?>" />
+			</p>
+
+			<p>
+				<label for="<?php echo $this->get_field_id('height'); ?>">Height:</label> 
+				<input class="widefat" id="<?php echo $this->get_field_id('height'); ?>" name="<?php echo $this->get_field_name('height'); ?>" type="text" value="<?php echo $height; ?>" />
+			</p>
+        <?php
+		
+	}//form
+	
+}
+add_action('widgets_init', create_function('', 'return register_widget("QassimFeaturedVideoWidget");') );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,80 @@
+﻿=== Featured Video Widget ===
+Contributors: Qassim.Dev
+Donate link: http://j.mp/plugins_donation
+Tags: video widget, responsive, responsive widget, responsive video, featured, featured video, featured video widget, featured widget, video, youtube, vimeo, image, images, shortcode, Post, plugin, posts, page, widget, admin, sidebar, google, twitter, comments
+Requires at least: 3.0
+Tested up to: 4.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Add featured Youtube or Vimeo video in your sidebar easily, responsive and customize height.
+
+== Description ==
+
+### Featured Video Widget  
+
+Add featured Youtube or Vimeo video in your sidebar easily, responsive and customize height.
+
+### The Features
+
+* Responsive Video.
+* Youtube And Vimeo Support.
+* Customize Height.
+* Easy.
+
+### Live Demo
+
+* [http://qass.im/my-plugins/](http://qass.im/my-plugins/)
+
+### Rate The Plugin
+
+* [Please rate Featured Video Widget plugin](https://wordpress.org/support/view/plugin-reviews/featured-video-widget#postform)
+
+### Download Zaman WordPress Blog Theme
+
+* [Responsive and Retina WordPress blog Theme with a lot of features](http://j.mp/zaman_theme)
+
+### Beautiful WordPress Themes
+
+* [Collection of 87 themes for only $69](http://j.mp/beautiful_themes)
+
+### My Premium WordPress Themes And Plugins
+
+* [My premium themes and plugins on Creative Market.](http://j.mp/1AS73zL)
+* [My premium themes and plugins on Themeforest.](http://j.mp/Perso_themes)
+
+### About
+
+* [The plugin designed and developed by Qassim Hassan.](http://qass.im)
+* [Qassim Hassan on Twitter.](https://twitter.com/Qassim_Dev)
+
+### More Plugins
+
+* [My Plugins](https://profiles.wordpress.org/qassimdev/#content-plugins)
+
+== Installation ==
+
+### Installation
+
+1. Upload 'featured-video-widget' folder to the '/wp-content/plugins/' directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Go to Appearance > Widgets > Featured Video.
+4. Drag and drop "Featured Video" widget and open it and enter video link, youtube or vimeo only.
+5. Change height if you want, default height is 220.
+6. Enjoy.
+
+== Frequently Asked Questions ==
+
+### FAQ
+
+* [For more questions or help, contact me.](http://qass.im/contact)
+* [Or ask me on twitter.](https://twitter.com/Qassim_Dev)
+
+== Changelog ==
+
+= 1.0.0 =
+First version.
+
+== Upgrade Notice ==
+Currently not available.
